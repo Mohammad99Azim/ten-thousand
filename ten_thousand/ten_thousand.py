6c590bb06461963a302bea832492989082047fd9
unknown <mohlebzo1@gmail.com>
2022-06-14 21:52:44 +0300

    Finished three scenarios, also refactored the calculate_score() function

---
+++ ten_thousand/ten_thousand.py
@@ -0,0 +1,56 @@
+from game_logic import GameLogic
+from banker import Banker
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def play(self, roller=GameLogic.roll_dice):
+        round_counter = 1
+        print('Welcome to Ten Thousand')
+        print('(y)es to play or (n)o to decline')
+        user_answer = input('> ')
+        if user_answer == 'n':
+            print('OK. Maybe another time')
+        else:
+            new_roller = Game.start_round_and_roll_dice(6, round_counter, roller)
+            print('Enter dice to keep, or (q)uit:')
+            user_answer = input('> ')
+            if user_answer == 'q':
+                print('Thanks for playing. You earned 0 points')
+            else:
+                shelved_amount = Banker()
+                round_score = GameLogic.calculate_score(tuple(user_answer))
+                shelved_amount.shelf(round_score)
+                print(f'You have {round_score} unbanked points and '
+                      f'{len(new_roller) - len(tuple(user_answer))} dice remaining')
+                print('(r)oll again, (b)ank your points or (q)uit:')
+                user_answer = input('> ')
+                if user_answer == 'q':
+                    print(f'Thanks for playing. You earned {round_score} points')
+                if user_answer == 'b':
+                    shelved_amount.bank()
+                    print(f'You banked {shelved_amount.balance} points in round {round_counter}')
+                    round_counter += 1
+                    print(f'Total score is {shelved_amount.balance} points')
+                    Game.start_round_and_roll_dice(6, round_counter, roller)
+                    print('Enter dice to keep, or (q)uit:')
+                    user_answer = input('> ')
+                    if user_answer == 'q':
+                        print('Thanks for playing. You earned 0 points')
+
+    @classmethod
+    def start_round_and_roll_dice(cls, dice_roll_num, round_number, roller):
+        print(f'Starting round {round_number}')
+        print('Rolling 6 dice...')
+        new_roller = roller(dice_roll_num)
+        formatted_roller = ' '.join([str(i) for i in new_roller])
+        print(f'*** {formatted_roller} ***')
+
+        return new_roller
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.play()
