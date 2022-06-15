fce97c2a8e19448de771cf410dfe5ee912805a3b
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-06-15 12:37:53 +0300

    add the bank_first_for_two_rounds  by Mohammad Azim

--- ten_thousand/ten_thousands.py
+++ ten_thousand/ten_thousands.py
@@ -1,4 +1,4 @@
-from game_logic import GameLogic
+from ten_thousand.game_logic import GameLogic
 from banker import Banker
 
 
@@ -40,6 +40,31 @@ class Game:
                     user_answer = input('> ')
                     if user_answer == 'q':
                         print(f'Thanks for playing. You earned {round_score} points')
+                        return
+
+
+                    # bank_first_for_two_rounds
+                    round_score = GameLogic.calculate_score(tuple(user_answer))
+                    shelved_amount.shelf(round_score)
+                    print(f'You have {round_score} unbanked points and '
+                          f'{len(new_roller) - len(tuple(user_answer))} dice remaining')
+                    dice_remaining = len(new_roller) - len(tuple(user_answer))
+                    print('(r)oll again, (b)ank your points or (q)uit:')
+                    user_answer = input('> ')
+                    if user_answer == 'q':
+                        print(f'Thanks for playing. You earned {round_score} points')
+
+                    if user_answer == 'b':
+                        shelved_amount.bank()
+                        print(f'You banked {shelved_amount.balance} points in round {round_counter}')
+                        round_counter += 1
+                        print(f'Total score is {shelved_amount.balance} points')
+                        Game.start_round_and_roll_dice(6, round_counter, roller)
+                        print('Enter dice to keep, or (q)uit:')
+                        user_answer = input('> ')
+                        if user_answer == 'q':
+                            print(f'Thanks for playing. You earned {shelved_amount.balance} points')
+                            return
 
                 # repeated_roller
                 if user_answer == 'r':
