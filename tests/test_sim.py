83dd9ef09f436bec3ec19cd66e2fa91555786964
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-06-16 09:47:41 +0300

    make the rolling_dice_text method

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -2,27 +2,32 @@ import pytest
 from tests.flo import diff
 from ten_thousand.ten_thousands import Game
 
+
 def test_quitter():
-    game=Game()
-    diffs =diff(game.play,"tests/version_2/quitter.sim.txt")
+    game = Game()
+    diffs = diff(game.play, "tests/version_2/quitter.sim.txt")
     assert not diffs, diffs
+
 
 def test_one_and_done():
-    game=Game()
-    diffs = diff(game.play,"tests/version_2/one_and_done.sim.txt")
+    game = Game()
+    diffs = diff(game.play, "tests/version_2/one_and_done.sim.txt")
     assert not diffs, diffs
 
+
 def test_bank_one_roll_then_quit():
-    game= Game()
-    diffs = diff(game.play,"tests/version_2/bank_one_roll_then_quit.sim.txt")
+    game = Game()
+    diffs = diff(game.play, "tests/version_2/bank_one_roll_then_quit.sim.txt")
     assert not diffs, diffs
+
 
 def test_bank_first_for_two_rounds():
     game = Game()
-    diffs = diff( game.play, "tests/version_2/bank_first_for_two_rounds.sim.txt"  )
+    diffs = diff(game.play, "tests/version_2/bank_first_for_two_rounds.sim.txt")
     assert not diffs, diffs
 
+
 def test_repeat_roller():
-    game= Game()
+    game = Game()
     diffs = diff(game.play, "tests/version_2/repeat_roller.sim.txt")
     assert not diffs, diffs
